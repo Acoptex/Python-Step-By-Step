70b46124c0e3f903ceaa33b11aa5a9e0c4776ca6
ALEKSANDR CUCHRIAJEV <113382837+Acoptex@users.noreply.github.com>
2022-09-17 14:46:16 +0300

    Add files via upload
    Added Lesson 4

---
+++ Part-1-Python-Basics/Lesson-4-Python-Variables-and-Naming-Conventions/Python-variables-and-naming-conventions.py
@@ -0,0 +1,30 @@
+# Python Variables and Naming Conventions
+
+# variable_name = value
+
+"""
+Naming variables
+The following are the variable rules that you should keep in mind:
+
+Variable names can contain only letters, numbers, and underscores (_).
+Variable names can start with a letter or an underscore (_), not with a number.
+Variable names cannot contain spaces. To separate words in variables, you use underscores for example list_of_names.
+Variable names cannot be the same as keywords, reserved words, and built-in functions in Python.
+Variable names are case-sensitive (name, Name and NAME are three different variables)
+Variable names should be concise and descriptive. For example, the user_name variable is more descriptive than the un.
+Use underscores (_) to separate multiple words in the variable names.
+Avoid using the letter l and the uppercase letter O because they look like the number 1 and 0.
+"""
+# Legal variable names:
+
+myname = "Alex"
+my_number = 11
+_my_float_number = 4.5
+isSunny = True
+COMPANYNAME = "ACOPTEX"
+myname2 = "Alex"
+
+# Illegal variable names:
+# 1myname = "Alex"
+# my-name = "Olga"
+# my name = "Victoria"
